37dcb647e383a85eb7e8d88d930cea652c256963
Ivan Panteliuk <i.v.pantelyuk@gmail.com>
2023-02-10 21:45:47 +0100

    Adding HW files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+const galleryItemsMarkup = makeGalleryCollectionMarkup(galleryItems);
+
+function makeGalleryCollectionMarkup(items) {
+  return items.map(({ preview, original, description } = {}) =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </div>`
+    )
+    .join('');
+}
+
+galleryListEl.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+galleryListEl.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `,
+    {
+      onShow: instance => window.addEventListener('keydown', closeModal),
+      onClose: instance => window.removeEventListener('keydown', closeModal),
+    },
+  );
+
+    instance.show();
+    
+    function closeModal(event) {
+      if (event.code === 'Escape') instance.close();
+    }
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+function makeGalleryCollectionMarkup (items) {
+    return items.map(({preview, original, description} = {}) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join('')
+}
+
+galleryListEl.innerHTML = makeGalleryCollectionMarkup(galleryItems)
+
+new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    scrollZoom: false
+});
+
